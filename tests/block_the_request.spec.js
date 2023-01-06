105a55cf6524fe3754a306ed27f2d0c495d6bc87
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-01-06 12:46:57 +0200

    specifying requests that need to be excluded

--- tests/block_the_request.spec.js
+++ tests/block_the_request.spec.js
@@ -1,9 +1,8 @@
-import {chromium, request, test} from '@playwright/test'
+import { chromium, request, test } from '@playwright/test'
 
-test('blocking google requests on the website', async({page}) => {
+test('blocking google requests on the website', async ({ page }) => {
 
-  //if we want to know what happens between requests back and fourth
-
+  // if we want to know what happens between requests back and fourth
   // page.on("request", req => console.log(`>> : ${req.method()} ${req.resourceType()} ${req.url()}`))
   // page.on("response", req => console.log(`<< : ${req.status()} ${req.url()}`))
 
@@ -25,14 +24,24 @@ test('blocking google requests on the website', async({page}) => {
   //   }
   // });
 
-  //if we want to exclude any url requests that contains word "google"
+  // if we want to exclude any url requests that contains word "google"
+  // await page.route("**/*", (route) => {
+  //   route.request().url().includes("google")
+  //      ? route.abort()
+  //      : route.continue();
+  //    return;
+  //  });
 
-  await page.route("**/*", (route) => {
-    route.request().url().includes("google")
-       ? route.abort()
-       : route.continue();
-     return;
-   });
+  // виключаємо конкретні запити
+  await page.route("**/*", (route, request) => {
+    const urls_to_block = ['https://www.googletagmanager.com/gtm.js?id=GTM-NDK3GF',
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDR2J9XFNnRowvm6NkU523oXyVaktVqgc8&libraries=places%2Cgeometry&language=uk&callback=Drupal.Jysk.googleMapsReadyCallback']
+    if (urls_to_block.includes(request.url())) {
+      route.abort();
+    } else {
+      route.continue();
+    }
+  });
 
   await page.goto('https://jysk.ua/')
   await page.getByRole('button', { name: 'Прийняти все' }).click();
