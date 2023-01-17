84d5ec1c35f025718ebe5e5874b271af883ccd20
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-01-17 14:50:28 +0200

    checked the results of my mock with screenshots

--- tests/mock_the_data.spec.js
+++ tests/mock_the_data.spec.js
@@ -9,16 +9,16 @@ test('mocking data test', async ({ page }) => {
   const logout = page.getByRole('link', { name: 'Logout' })
 
   await page.route("**/*", (route, request) => {
-    const mocking_urls = ['https://www.saucedemo.com/static/media/Login_Bot_graphic.20658452.png',
+    const mockingUrls = new Set(['https://www.saucedemo.com/static/media/Login_Bot_graphic.20658452.png',
       'https://www.saucedemo.com/static/media/red-tatt-1200x1500.e32b4ef9.jpg',
       'https://www.saucedemo.com/static/media/red-onesie-1200x1500.1b15e1fa.jpg',
       'https://www.saucedemo.com/static/media/sauce-pullover-1200x1500.439fc934.jpg',
       'https://www.saucedemo.com/static/media/bolt-shirt-1200x1500.c0dae290.jpg',
       'https://www.saucedemo.com/static/media/bike-light-1200x1500.a0c9caae.jpg',
       'https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.34e7aa42.jpg',
-    ]
+    ])
 
-    if (mocking_urls.includes(request.url())) {
+    if (mockingUrls.has(request.url())) {
       route.fulfill({
         contentType: 'image/jpeg',
         path: 'tests/images/crab-icon.png',
@@ -29,17 +29,22 @@ test('mocking data test', async ({ page }) => {
   })
 
   await page.goto('https://www.saucedemo.com')
+
   await expect(page).toHaveTitle('Swag Labs')
+  expect(await page.screenshot({fullPage: true})).toMatchSnapshot();
 
-  await username.click();
-  await username.fill('standard_user');
-  await password.click();
-  await password.fill('secret_sauce');
-  await login.click();
-  await page.pause();
+  await username.click()
+  await username.fill('standard_user')
+  await password.click()
+  await password.fill('secret_sauce')
+  await login.click()
+
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+  expect(await page.locator('.inventory_list').screenshot()).toMatchSnapshot();
 
-  await burger.click();
-  await logout.click();
+  await burger.click()
+  await logout.click()
+
   await expect(page).toHaveURL('https://www.saucedemo.com')
+  expect(await page.screenshot({fullPage: true})).toMatchSnapshot();
 })
