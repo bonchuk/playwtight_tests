e77eeb43ddddc02bfb560c625830a2bbaeac3762
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-01-27 14:34:16 +0200

    added cookie to avoid pop up authorization window

--- tests/third_party_services_login.spec.js
+++ tests/third_party_services_login.spec.js
@@ -1,6 +1,14 @@
 import { test } from '@playwright/test'
 
-test('login test', async ({ page }) => {
+test('login test', async ({ browser }) => {
+
+  // Create a new incognito browser context
+  const context = await browser.newContext();
+  // Create a new page inside context.
+  const page = await context.newPage();
+  // add cookies
+  await context.addCookies([{ name: "NOT_VISIBLE_MODAL", value: "true", domain: "megasport.ua", path: "/", expires: 1675419510, secure: true, sameSite: "None" }]);
+
   await page.route("**/*", (route, request) => {
     const urlsToBlock = new Set(
       [
@@ -11,25 +19,22 @@ test('login test', async ({ page }) => {
       ])
     if (urlsToBlock.has(request.url())) {
       route.abort();
-    } else if (request.url() === 'https://megasport.ua/31035/dist/js/358.chunk.js') {
-      page.locator('.k56WGs > button').click();
     } else {
       route.continue();
     }
   });
 
-
   await page.goto('https://megasport.ua/ua/authorization/');
+
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('button', { name: 'Google' }).click();
   const page1 = await page1Promise;
   await page1.getByRole('textbox', { name: 'Електронна адреса або номер телефону' }).fill('memcrab.test@gmail.com');
   await page1.getByRole('button', { name: 'Далі' }).click();
-  await page1.getByRole('textbox', { name: 'Введіть пароль' }).fill('****************');
+  await page1.getByRole('textbox', { name: 'Введіть пароль' }).fill('cigto2-coPmor-xizgut');
 
-  const navigationPromise = page.waitForNavigation();
   await page1.locator('#passwordNext').click()
-  await navigationPromise;
+  await page.waitForNavigation();
 
   await page.locator('._O0dT7 > a').first().click();
   await page.getByRole('button', { name: 'Вийти' }).click();
