83dcf1f14b85b36f741a12a398ae9d5470afd5f7
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-01-31 15:02:29 +0200

    update gitignore and config files

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,20 @@
 // @ts-check
 const { devices } = require('@playwright/test');
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  webServer: [
+    {
+      command: 'npm run start',
+      port: 3000,
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    }
+  ],
+  use: {
+    baseURL: 'http://localhost:3000/',
+  },
+});
 
 /**
  * Read environment variables from file.
@@ -43,7 +58,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
+    // baseURL: 'http://localhost:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -106,15 +121,10 @@ const config = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  // webServer: {
     // command: 'npm run dev --port 8080',
     // port: 8080,
     // reuseExistingServer: true
-    command: 'npm run start',
-    url: 'http://localhost:3000/',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
 };
 
 module.exports = config;
