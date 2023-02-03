d70d03deebb505271412bcf05230089682cdbd4b
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-02-03 12:03:57 +0200

    increasing timeout for expect

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 15000
+    timeout: 30000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
