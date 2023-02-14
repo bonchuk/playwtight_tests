762a28ab7df9f6f424a2a2b5ccf74bb6a3d1fdc4
Oleh Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2023-02-14 15:22:26 +0200

    redid the сode

--- tests/hooks_and_groups.spec.js
+++ tests/hooks_and_groups.spec.js
@@ -1,24 +1,38 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('PC assembly', () => {
+  // let count = 0;
+
+  let links_to_block = new Set([])
 
   test.beforeEach(async ({ page }) => {
 
     await page.route("**/*", (route, request) => {
-      const urlsToBlock = new Set(['https://www.googletagservices.com/tag/js/gpt.js',
-        'https://www.googletagmanager.com/gtm.js?id=GTM-N4LDSTX',
-        'https://www.google-analytics.com/gtm/optimize.js?id=GTM-PN45D27',
-        'https://www.google-analytics.com/analytics.js',
-        'https://googleads.g.doubleclick.net/pagead/viewthroughconversion/964380755/?random=1673617612808&cv=11&fst=1673617612808&bg=ffffff&guid=ON&async=1&gtm=2wg1a1&u_w=1280&u_h=720&hn=www.googleadservices.com&frm=0&url=https%3A%2F%2Frozetka.com.ua%2Fua%2F&tiba=%D0%86%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD%20ROZETKA%E2%84%A2%3A%20%D0%BE%D1%84%D1%96%D1%86%D1%96%D0%B9%D0%BD%D0%B8%D0%B9%20%D1%81%D0%B0%D0%B9%D1%82%20%D0%BD%D0%B0%D0%B9%D0%BF%D0%BE%D0%BF%D1%83%D0%BB%D1%8F&auid=831961265.1673613958&uaa=arm&uab=64&uafvl=Chromium%3B109.0.5414.46%7CNot_A%2520Brand%3B99.0.0.0&uap=macOS&uapv=12.6.0&uaw=0&data=ecomm_pagetype%3DMain%3Becomm_totalvalue%3D0%3Bdynx_totalvalue%3D0%3Bdynx_pagetype%3DMain%3Bdynx_itemid2%3D0%3Becomm_pcat%3D(not%20set)%3Becomm_pname%3D(not%20set)&rfmt=3&fmt=4',
-        'https://googleads.g.doubleclick.net/pagead/viewthroughconversion/967924631/?random=1673617612811&cv=11&fst=1673617612811&bg=ffffff&guid=ON&async=1&gtm=2wg1a1&u_w=1280&u_h=720&label=K55HCOH94wMQl7fFzQM&hn=www.googleadservices.com&frm=0&url=https%3A%2F%2Frozetka.com.ua%2Fua%2F&tiba=%D0%86%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD%20ROZETKA%E2%84%A2%3A%20%D0%BE%D1%84%D1%96%D1%86%D1%96%D0%B9%D0%BD%D0%B8%D0%B9%20%D1%81%D0%B0%D0%B9%D1%82%20%D0%BD%D0%B0%D0%B9%D0%BF%D0%BE%D0%BF%D1%83%D0%BB%D1%8F&value=0&auid=831961265.1673613958&uaa=arm&uab=64&uafvl=Chromium%3B109.0.5414.46%7CNot_A%2520Brand%3B99.0.0.0&uap=macOS&uapv=12.6.0&uaw=0&data=prodid%3D(not%20set)%3Bpname%3D(not%20set)%3Bpcat%3D(not%20set)&rfmt=3&fmt=4',
-        'https://googleads.g.doubleclick.net/pagead/viewthroughconversion/529222180/?random=1673617612827&cv=11&fst=1673617612827&bg=ffffff&guid=ON&async=1&gtm=2wg1a1&u_w=1280&u_h=720&hn=www.googleadservices.com&frm=0&url=https%3A%2F%2Frozetka.com.ua%2Fua%2F&tiba=%D0%86%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD%20ROZETKA%E2%84%A2%3A%20%D0%BE%D1%84%D1%96%D1%86%D1%96%D0%B9%D0%BD%D0%B8%D0%B9%20%D1%81%D0%B0%D0%B9%D1%82%20%D0%BD%D0%B0%D0%B9%D0%BF%D0%BE%D0%BF%D1%83%D0%BB%D1%8F&auid=831961265.1673613958&uaa=arm&uab=64&uafvl=Chromium%3B109.0.5414.46%7CNot_A%2520Brand%3B99.0.0.0&uap=macOS&uapv=12.6.0&uaw=0&data=ecomm_pagetype%3DMain%3Becomm_totalvalue%3D0%3Becomm_pcat%3D(not%20set)%3Becomm_pname%3D(not%20set)&rfmt=3&fmt=4']);
 
-      if (urlsToBlock.has(request.url())) {
+      const url = [
+        'https://adservice.',
+        'https://googleads.',
+        'https://www.googletagservices.',
+        'https://www.googletagmanager.',
+        'https://www.google-analytics.',
+        'https://www.google.com.ua/ads/'
+      ];
+
+      for (let link of url) {
+        if (request.url().startsWith(link)) {
+          links_to_block.add(request.url())
+        }
+      }
+
+      if (links_to_block.has(request.url())) {
         route.abort();
       } else {
         route.continue();
       }
     });
+
+    // page.on("response", req => console.log(`<< : ${req.url().includes('google') ? count++ && req.url() : false
+    //   }`))
 
     await page.goto('https://rozetka.com.ua/ua/')
     await page.getByRole('link', { name: 'Товари для геймерів' }).click();
@@ -28,10 +42,11 @@ test.describe('PC assembly', () => {
   })
 
   test.afterEach(async ({ page }) => {
+    // console.log(count);
     await page.close()
   })
 
-  test('picking a motherboard', async ({ page }) => {
+  test.only('picking a motherboard', async ({ page }) => {
 
     await page.locator('.tile-cats').filter({ hasText: 'Материнські плати' }).click()
     await page.locator('.goods-tile__title').filter({ hasText: 'Материнська плата Asus TUF Gaming B550-Plus (sAM4, AMD B550, PCI-Ex16)' }).click()
